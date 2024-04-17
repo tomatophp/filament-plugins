e60d9c47d9e3e68b068b58f347391f4c2747fa74
Eelco <eelco@syzygy.eu>
2024-04-17 17:05:59 +0200

    trans in blade echo wrapper

--- resources/views/pages/table.blade.php
+++ resources/views/pages/table.blade.php
@@ -29,7 +29,7 @@
             <div class="flex justifiy-between gap-1 my-4 px-4 border-t border-gray-100 dark:border-gray-700 pt-4">
                 <div class="flex justifiy-start w-full gap-2">
                     @if((bool)config('filament-plugins.allow_generator'))
-                        <x-filament::icon-button tooltip="trans('filament-plugins::messages.plugins.generate')" tag="a" href="{{route('filament.'.filament()->getCurrentPanel()->getId().'.resources.tables.index', ['module'=>$item->module_name])}}">
+                        <x-filament::icon-button tooltip="{{ trans('filament-plugins::messages.plugins.generate') }}" tag="a" href="{{route('filament.'.filament()->getCurrentPanel()->getId().'.resources.tables.index', ['module'=>$item->module_name])}}">
                             <x-slot name="icon">
                                 <x-heroicon-s-cog class="w-5 h-5" />
                             </x-slot>
