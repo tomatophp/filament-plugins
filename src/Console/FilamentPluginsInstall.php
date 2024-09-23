91fc5bd56ab74279c991e95b0ce5463adcfcd256
Fady Mondy <info@3x1.io>
2024-09-23 16:44:18 +0300

    add filament:plugins command and filament-plugins:list

--- src/Console/FilamentPluginsInstall.php
+++ src/Console/FilamentPluginsInstall.php
@@ -3,11 +3,13 @@
 namespace TomatoPHP\FilamentPlugins\Console;
 
 use Illuminate\Console\Command;
+use TomatoPHP\ConsoleHelpers\Traits\HandleFiles;
 use TomatoPHP\ConsoleHelpers\Traits\RunCommand;
 
 class FilamentPluginsInstall extends Command
 {
     use RunCommand;
+    use HandleFiles;
 
     /**
      * The name and signature of the console command.
@@ -38,7 +40,8 @@ class FilamentPluginsInstall extends Command
     {
         $this->info('Publish Vendor Assets');
         $this->artisanCommand(["migrate"]);
-        $this->artisanCommand(["optimize:clear"]);
+        $this->copyFile(__DIR__.'/../../publish/modules.php', config_path('modules.php'));
+        $this->artisanCommand(["filament:optimize"]);
         $this->info('Filament Plugins installed successfully.');
     }
 }
